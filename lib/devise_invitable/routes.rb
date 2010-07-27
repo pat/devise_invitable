ac676df9b659916633a89ed477ab3528d219b75d
Pat Allan <pat@freelancing-gods.com>
2010-07-27 18:05:45 +1000

    Updating to latest route syntax - I think I've got it right. Also, adding in a require for the controller helpers. Not sure why they weren't being required to start with.

--- lib/devise_invitable/routes.rb
+++ lib/devise_invitable/routes.rb
@@ -1,18 +1,15 @@
 module ActionDispatch
   module Routing
     class Mapper
-    
-    protected
+      protected
       
       def devise_invitation(mapping, controllers)
-        scope mapping.full_path[1..-1], :name_prefix => mapping.name do
-          resource :invitation, :only => [:new, :create, :edit, :update], :path => mapping.path_names[:invitation],
-                   :controller => controllers[:invitations] do
+        scope :controller => controllers[:invitations] do
+          resource :invitation, :only => [:new, :create, :edit, :update], :path => mapping.path_names[:invitation] do
             get mapping.path_names[:accept].to_sym, :action => :edit
           end
         end
       end
-      
     end
   end
 end
