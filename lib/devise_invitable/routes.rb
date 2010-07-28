c3bc2d1a2e21bfcdaae2f5732885f699e8d20549
Pat Allan <pat@freelancing-gods.com>
2010-07-28 14:34:21 +1000

    Fixing routes.

--- lib/devise_invitable/routes.rb
+++ lib/devise_invitable/routes.rb
@@ -4,10 +4,8 @@ module ActionDispatch
       protected
       
       def devise_invitation(mapping, controllers)
-        scope :controller => controllers[:invitations] do
-          resource :invitation, :only => [:new, :create, :edit, :update], :path => mapping.path_names[:invitation] do
-            get mapping.path_names[:accept].to_sym, :action => :edit
-          end
+        resource :invitation, :only => [:new, :create, :edit, :update], :path => mapping.path_names[:invitation], :controller => controllers[:invitations] do
+          get mapping.path_names[:accept].to_sym, :action => :edit
         end
       end
     end
